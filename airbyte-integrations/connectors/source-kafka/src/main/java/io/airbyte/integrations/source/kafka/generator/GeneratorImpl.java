33f210474be919a98a4176c721c26dce6646e67b
Luis Vicente <luis.vicente@prima.it>
2023-06-02 16:08:58 +0100

    SSDP-174 Reaching feature parity with previous implementation

--- airbyte-integrations/connectors/source-kafka/src/main/java/io/airbyte/integrations/source/kafka/generator/GeneratorImpl.java
+++ airbyte-integrations/connectors/source-kafka/src/main/java/io/airbyte/integrations/source/kafka/generator/GeneratorImpl.java
@@ -4,26 +4,36 @@ import com.google.common.collect.AbstractIterator;
 import io.airbyte.commons.util.AutoCloseableIterator;
 import io.airbyte.commons.util.AutoCloseableIterators;
 import io.airbyte.integrations.source.kafka.converter.Converter;
+import io.airbyte.integrations.source.kafka.format.AvroFormat;
 import io.airbyte.integrations.source.kafka.mediator.KafkaMediator;
 import io.airbyte.protocol.models.v0.AirbyteMessage;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public abstract class AbstractGenerator<V> implements Generator {
+public class GeneratorImpl<V> implements Generator {
 
   private final KafkaMediator<V> mediator;
   private final Converter<V> converter;
+  private final int maxRecords;
 
-  public AbstractGenerator(KafkaMediator<V> mediator, Converter<V> converter) {
+  private static final Logger LOGGER = LoggerFactory.getLogger(GeneratorImpl.class);
+
+  public GeneratorImpl(KafkaMediator<V> mediator, Converter<V> converter, int maxRecords) {
     this.mediator = mediator;
     this.converter = converter;
+    this.maxRecords = maxRecords;
   }
 
   @Override
-  public AutoCloseableIterator<AirbyteMessage> read() {
+  final public AutoCloseableIterator<AirbyteMessage> read() {
 
     return AutoCloseableIterators.fromIterator(new AbstractIterator<>() {
+
+      private int totalRead = 0;
 
       final Queue<ConsumerRecord<String, V>> buffer = new LinkedList<>();
 
@@ -32,7 +42,18 @@ public abstract class AbstractGenerator<V> implements Generator {
 
         // Try to load a new batch if buffer is empty
         if (buffer.isEmpty()) {
-          buffer.addAll(mediator.poll());
+          // Only load a new batch if we haven't reached max_records
+          if (this.totalRead < GeneratorImpl.this.maxRecords) {
+            List<ConsumerRecord<String, V>> batch;
+            var nrOfRetries = 0;
+            do {
+              batch = mediator.poll();
+              totalRead += batch.size();
+              buffer.addAll(batch);
+            } while (batch.size() == 0 && ++nrOfRetries < 10);
+          } else {
+            return endOfData();
+          }
         }
 
         // If it's still empty, no more data to consume
@@ -43,7 +64,6 @@ public abstract class AbstractGenerator<V> implements Generator {
           return converter.convertToAirbyteRecord(message.topic(), message.value());
         }
       }
-
     });
   }
 }
