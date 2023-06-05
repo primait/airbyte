c969f87789aac45be99757b04abcbd10fd673525
Luis Vicente <luis.vicente@prima.it>
2023-06-05 17:00:48 +0100

    SSDP-174 Rewriting serialisation test

--- airbyte-integrations/connectors/source-kafka/src/test/java/io/airbyte/integrations/source/kafka/state/StateHelperTest.java
+++ airbyte-integrations/connectors/source-kafka/src/test/java/io/airbyte/integrations/source/kafka/state/StateHelperTest.java
@@ -18,33 +18,42 @@ public class StateHelperTest {
 
   @Test
   public void testGlobalStateDeserialisation() throws IOException {
-    final var jsonState = Jsons.deserialize(MoreResources.readResource("state/test_global_state.json"));
+    final var jsonState = Jsons.deserialize(MoreResources.readResource("state/test_global_state_deserialisation.json"));
     final var state = StateHelper.stateFromJson(jsonState);
-    final var expected = Map.of(new TopicPartition("topic-0", 0), 42L);
+    final var expected = Map.ofEntries(
+        Map.entry(new TopicPartition("topic-0", 0), 42L)
+    );
     assertEquals(expected, state);
   }
 
   @Test
   public void testLegacyStateDeserialisation() throws IOException {
-    final var jsonState = Jsons.deserialize(MoreResources.readResource("state/test_legacy_state.json"));
+    final var jsonState = Jsons.deserialize(MoreResources.readResource("state/test_legacy_state_deserialisation.json"));
     final var state = StateHelper.stateFromJson(jsonState);
     assertTrue(state.isEmpty());
   }
 
   @Test
   public void testStreamStateDeserialisation() throws IOException {
-    final var jsonState = Jsons.deserialize(MoreResources.readResource("state/test_stream_state.json"));
+    final var jsonState = Jsons.deserialize(MoreResources.readResource("state/test_stream_state_deserialisation.json"));
     final var state = StateHelper.stateFromJson(jsonState);
-    final var expected = Map.of(new TopicPartition("topic-1", 0), 24L, new TopicPartition("topic-1", 1), 42L);
+    final var expected = Map.ofEntries(
+        Map.entry(new TopicPartition("topic-1", 0), 24L),
+        Map.entry(new TopicPartition("topic-1", 1), 42L)
+    );
     assertEquals(expected, state);
   }
 
   @Test
   public void testStateSerialisation() throws IOException {
-    final var initial = Map.of(new TopicPartition("topic-0", 0), 24L, new TopicPartition("topic-1", 0), 42L, new TopicPartition("topic-1", 1), 66L);
-    final var serialised = Jsons.jsonNode(StateHelper.toAirbyteState(initial));
-    final var recovered = StateHelper.stateFromJson(serialised);
-    assertEquals(initial, recovered);
+    final var state = Map.ofEntries(
+        Map.entry(new TopicPartition("topic-0", 0), 24L),
+        Map.entry(new TopicPartition("topic-1", 0), 42L),
+        Map.entry(new TopicPartition("topic-1", 1), 66L)
+    );
+    final var serialised = Jsons.serialize(StateHelper.toAirbyteState(state));
+    final var expected = MoreResources.readResource("state/test_state_serialisation.json");
+    assertEquals(expected, serialised);
   }
 
 }
